d385933370489afc1045c660c374a668c0a9bee4
Logan Stack <loganestack@gmail.com>
2023-12-05 21:50:15 -0500

    Fixed font-sizing inconsistencies/removed clamps.

--- src/components/Info.js
+++ src/components/Info.js
@@ -9,7 +9,7 @@ function Info({activeTab}) {
         return (
           <div className="info">
             <p className="info_header">Hello.</p>
-            <p>
+            <p className="info_description">
               My name's Logan and I'm a software engineer based in Detroit, MI with a passion for creativity.
               I'm currently employed at General Motors, where I write software to test the cameras 
               that go into our autonomous vehicles. You can read about my past work experience below 
@@ -17,13 +17,13 @@ function Info({activeTab}) {
             </p>
             <div>
               <a className="font_awesome_icons" href="mailto:loganestack@gmail.com" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faEnvelope} style={{"fontSize":"1.3em"}} />
+                <FontAwesomeIcon icon={faEnvelope}  />
               </a>
               <a className="font_awesome_icons" href="https://www.linkedin.com/in/logan-stack/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} style={{"fontSize":"1.3em"}} />
+                <FontAwesomeIcon icon={faLinkedin} />
               </a>
               <a className="font_awesome_icons" href="https://github.com/LoganEStack" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} style={{"fontSize":"1.3em"}} />
+                <FontAwesomeIcon icon={faGithub} />
               </a>            
             </div>
           </div>
@@ -31,40 +31,40 @@ function Info({activeTab}) {
       case "Work":
         return (
           <div className="info">
-            <div style={{"paddingBottom": "1.2em"}}>
+            <div className="info_section">
               <p className="info_header"><a href="https://www.gm.com/" target="_blank" rel="noopener noreferrer">General Motors</a></p>
-              <p className="role">Software Engineer <span style={{"color":"grey"}}>(2022-Present)</span></p>
-              <p className="role_description">I write software to analyze image quality for the cameras on GM's autonomous vehicles.</p>
-              <p className="role">DevOps Engineer <span style={{"color":"grey"}}>(2019-2022)</span></p>
-              <p className="role_description">I worked on the automation of build and testing software for the code residing in 60 individual Electronic Control Units for the Vehicles and Embedded Controls division.</p>
+              <p className="info_description">Software Engineer <span style={{"color":"grey"}}>(2022-Present)</span></p>
+              <p className="info_subdescription">I write software to analyze image quality for the cameras on GM's autonomous vehicles.</p>
+              <p className="info_description">DevOps Engineer <span style={{"color":"grey"}}>(2019-2022)</span></p>
+              <p className="info_subdescription">I worked on the automation of build and testing software for the code residing in 60 individual Electronic Control Units for the Vehicles and Embedded Controls division.</p>
             </div>
-            <div style={{"paddingBottom": "1.2em"}}>
+            <div className="info_section">
               <p className="info_header"><a href="https://www.michelin.com/en/" target="_blank" rel="noopener noreferrer">Michelin</a></p>
-              <p className="role">Software Engineer Intern <span style={{"color":"grey"}}>(2018)</span></p>
-              <p className="role_description">I spent my time on the Dev Ops Team writing applications to support other sub-departments within IT.</p>
+              <p className="info_description">Software Engineer Intern <span style={{"color":"grey"}}>(2018)</span></p>
+              <p className="info_subdescription">I spent my time on the Dev Ops Team writing applications to support other sub-departments within IT.</p>
             </div>
-            <div style={{"paddingBottom": "1.2em"}}>
+            <div className="info_section">
               <p className="info_header"><a href="https://www.ieee.org/" target="_blank" rel="noopener noreferrer">IEEE</a></p>
-              <p className="role">Software Engineer Intern <span style={{"color":"grey"}}>(2017)</span></p>
-              <p className="role_description">I built and maintained an internal IT Portal for department-wide use using WordPress.</p>
+              <p className="info_description">Software Engineer Intern <span style={{"color":"grey"}}>(2017)</span></p>
+              <p className="info_subdescription">I built and maintained an internal IT Portal for department-wide use using WordPress.</p>
             </div>
           </div>
         );
       case "Projects":
         return (
           <div className="info">
-            <div style={{"paddingBottom": "1.2em"}}>
+            <div className="info_section">
               <p className="info_header"><a href="https://github.com/LoganEStack/picture_book" target="_blank" rel="noopener noreferrer">picture_book</a></p>
-              <p className="role_description">A program that adds AI generated illustrations to books by using topic 
+              <p className="info_subdescription">A program that adds AI generated illustrations to books by using topic 
                 modeling to determine the central theme of each page.</p>
             </div>
-            <div style={{"paddingBottom": "1.2em"}}>
+            <div className="info_section">
               <p className="info_header"><a href="https://github.com/LoganEStack/FakeFactsBot" target="_blank" rel="noopener noreferrer">Twitter Bot</a></p>
-              <p className="role_description">Wrote a twitter bot that tweets comments from reddit.com threads every hour using twitter and reddit API.</p>
+              <p className="info_subdescription">Wrote a twitter bot that tweets comments from reddit.com threads every hour using twitter and reddit API.</p>
             </div>
-            <div style={{"paddingBottom": "1.2em"}}>
+            <div className="info_section">
               <p className="info_header"><a href="https://github.com/LoganEStack/PhysicianTimeTracker" target="_blank" rel="noopener noreferrer">Physician Time Tracker</a></p>
-              <p className="role_description">Worked with a capstone team to create a Swift application for Shippert Tech. This application is capable of recording medical procedures and time spent with patients for auditing purposes.</p>
+              <p className="info_subdescription">Worked with a capstone team to create a Swift application for Shippert Tech. This application is capable of recording medical procedures and time spent with patients for auditing purposes.</p>
             </div>
           </div>
         );
@@ -72,8 +72,10 @@ function Info({activeTab}) {
         return (
           <div className="info">
             <p className="info_header">See some of my art.</p>
-            <p style={{"marginBottom": "0em"}}>Drawing and painting are hobbies of mine.</p>
-            <p>See some of the stuff I've done by clicking bellow.</p>
+            <p className="info_description">
+              <p style={{"marginBottom": "0em"}}>Drawing and painting are hobbies of mine.</p>
+              <p>See some of the stuff I've done by clicking bellow.</p>
+            </p>
             <a href="https://drive.google.com/drive/folders/1qQzT3iMG80d-Zth5t1t94XKaxtcF7PGD?usp=sharing" target="_blank" rel="noopener noreferrer">
               <Button variant="outline-dark" className="rounded-0">See some stuff</Button>
             </a>
--- src/components/Name.js
+++ src/components/Name.js
@@ -3,13 +3,13 @@ import resume from "../assets/Logan_Stack_Resume.pdf"
 
 function Name() {
   return (
-    <div className="name" style={{"padding":".5rem 1rem"}}>
+    <div className="name">
       <div className="name_outline"/>
       <div className="name_outline"/>
       <div className="name_outline"/>
       <div className="name_outline"/>
-      <p style={{"fontWeight": "bold", "fontSize": "1.5em"}}>Logan Stack</p>
-      <p style={{"fontSize": "1.1em"}}>Software Engineer at General Motors</p>
+      <p className="name_header">Logan Stack</p>
+      <p className="name_subheader">Software Engineer at General Motors</p>
       <a href={resume} target="_blank" rel="noopener noreferrer">
         <Button variant="outline-dark" className="rounded-0">Grab a resume</Button>
       </a>
