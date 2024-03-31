f923fbf909cfc27900162086c25d46a5bf5eb34d
Logan Stack <loganestack@gmail.com>
2024-03-31 15:11:20 -0400

    Homepage done.

--- src/pages/Art.js
+++ src/pages/Art.js
@@ -1,147 +1,147 @@
-import { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import palette from '../assets/images/palette.png';
-import two_piece_abstract from '../assets/images/art/2 Piece Abstract.jpg';
-import three_piece_abstract from '../assets/images/art/3 Piece Abstract.jpg';
-import bill_murray from '../assets/images/art/Bill Murray.jpg';
-import bird_with_arms from '../assets/images/art/Bird with Arms.jpg';
-import chaplin from '../assets/images/art/Chaplin.png';
-import dog_tags from '../assets/images/art/Dog Tags.png';
-import dog from '../assets/images/art/Dog.jpg';
-import dragonball from '../assets/images/art/Dragonball.jpg';
-import fireball from '../assets/images/art/Fireball.jpg';
-import flower from '../assets/images/art/flower.jpg';
-import gorilla from '../assets/images/art/Gorilla.png';
-import nachopus from '../assets/images/art/Nachopus.jpg';
-import six_shooter from '../assets/images/art/Six Shooter.png';
-import spiderman from '../assets/images/art/Spiderman vs Wolverine.jpg';
-import trex from '../assets/images/art/T-Rex Moped.png';
-import toys from '../assets/images/art/Toys.jpg';
-import tucan from '../assets/images/art/Tucan.jpg';
-import yoshi from '../assets/images/art/Yoshi.jpg';
+// import { useState } from 'react';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import palette from '../assets/images/palette.png';
+// import two_piece_abstract from '../assets/images/art/2 Piece Abstract.jpg';
+// import three_piece_abstract from '../assets/images/art/3 Piece Abstract.jpg';
+// import bill_murray from '../assets/images/art/Bill Murray.jpg';
+// import bird_with_arms from '../assets/images/art/Bird with Arms.jpg';
+// import chaplin from '../assets/images/art/Chaplin.png';
+// import dog_tags from '../assets/images/art/Dog Tags.png';
+// import dog from '../assets/images/art/Dog.jpg';
+// import dragonball from '../assets/images/art/Dragonball.jpg';
+// import fireball from '../assets/images/art/Fireball.jpg';
+// import flower from '../assets/images/art/flower.jpg';
+// import gorilla from '../assets/images/art/Gorilla.png';
+// import nachopus from '../assets/images/art/Nachopus.jpg';
+// import six_shooter from '../assets/images/art/Six Shooter.png';
+// import spiderman from '../assets/images/art/Spiderman vs Wolverine.jpg';
+// import trex from '../assets/images/art/T-Rex Moped.png';
+// import toys from '../assets/images/art/Toys.jpg';
+// import tucan from '../assets/images/art/Tucan.jpg';
+// import yoshi from '../assets/images/art/Yoshi.jpg';
 
-const Art = () => {
-  const [popupImage, setPopupImage] = useState(null);
+// const Art = () => {
+//   const [popupImage, setPopupImage] = useState(null);
 
-  const openPopup = (imageSrc) => {
-    setPopupImage(imageSrc);
-  };
+//   const openPopup = (imageSrc) => {
+//     setPopupImage(imageSrc);
+//   };
 
-  const closePopup = () => {
-    setPopupImage(null);
-  };
+//   const closePopup = () => {
+//     setPopupImage(null);
+//   };
 
-  return (
-    <Container fluid className="vh-100 d-flex flex-column ">
-      <Row className="h-100">
-        <Col style={{ "margin": "auto" }}>
-          <Row>
-            <img className="image_palette" src={palette} alt="palette drawing" />
-          </Row>
-          <Row>
-            <div className="info_section">
-              <p className="info_header">Art</p>
-              <p className="info_description">
-                Drawing and painting is a hobby of mine. A majority of my experience is in acrylics and pen and ink,
-                but I'm always trying out new mediums to see if something sticks.
-                Check out some of the stuff I've done here.
-              </p>
-            </div>
-          </Row>
-        </Col>
-        <Col>
-          <div className="art_section">
-            <div className="artwork_scroll_box">
-              <div class="scrollbar" id="scrollbar1">
-                <div className='artwork'>
-                  <img src={bill_murray} alt="Bill Murray art" onClick={() => openPopup(bill_murray)} />
-                </div>
+//   return (
+//     <Container fluid className="vh-100 d-flex flex-column ">
+//       <Row className="h-100">
+//         <Col style={{ "margin": "auto" }}>
+//           <Row>
+//             <img className="image_palette" src={palette} alt="palette drawing" />
+//           </Row>
+//           <Row>
+//             <div className="info_section">
+//               <p className="info_header">Art</p>
+//               <p className="info_description">
+//                 Drawing and painting is a hobby of mine. A majority of my experience is in acrylics and pen and ink,
+//                 but I'm always trying out new mediums to see if something sticks.
+//                 Check out some of the stuff I've done here.
+//               </p>
+//             </div>
+//           </Row>
+//         </Col>
+//         <Col>
+//           <div className="art_section">
+//             <div className="artwork_scroll_box">
+//               <div class="scrollbar" id="scrollbar1">
+//                 <div className='artwork'>
+//                   <img src={bill_murray} alt="Bill Murray art" onClick={() => openPopup(bill_murray)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={bird_with_arms} alt="bird with arms art" onClick={() => openPopup(bird_with_arms)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={bird_with_arms} alt="bird with arms art" onClick={() => openPopup(bird_with_arms)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={two_piece_abstract} alt="two piece abstract art" onClick={() => openPopup(two_piece_abstract)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={two_piece_abstract} alt="two piece abstract art" onClick={() => openPopup(two_piece_abstract)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={three_piece_abstract} alt="three piece abstract art" onClick={() => openPopup(three_piece_abstract)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={three_piece_abstract} alt="three piece abstract art" onClick={() => openPopup(three_piece_abstract)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={chaplin} alt="Chaplin art" onClick={() => openPopup(chaplin)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={chaplin} alt="Chaplin art" onClick={() => openPopup(chaplin)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={dog_tags} alt="dog_tags drawing" onClick={() => openPopup(dog_tags)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={dog_tags} alt="dog_tags drawing" onClick={() => openPopup(dog_tags)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={dog} alt="dog drawing" onClick={() => openPopup(dog)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={dog} alt="dog drawing" onClick={() => openPopup(dog)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={dragonball} alt="dragonball drawing" onClick={() => openPopup(dragonball)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={dragonball} alt="dragonball drawing" onClick={() => openPopup(dragonball)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={fireball} alt="fireball drawing" onClick={() => openPopup(fireball)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={fireball} alt="fireball drawing" onClick={() => openPopup(fireball)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={flower} alt="flower drawing" onClick={() => openPopup(flower)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={flower} alt="flower drawing" onClick={() => openPopup(flower)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={gorilla} alt="gorilla drawing" onClick={() => openPopup(gorilla)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={gorilla} alt="gorilla drawing" onClick={() => openPopup(gorilla)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={nachopus} alt="nachopus drawing" onClick={() => openPopup(nachopus)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={nachopus} alt="nachopus drawing" onClick={() => openPopup(nachopus)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={six_shooter} alt="six_shooter drawing" onClick={() => openPopup(six_shooter)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={six_shooter} alt="six_shooter drawing" onClick={() => openPopup(six_shooter)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={spiderman} alt="spiderman drawing" onClick={() => openPopup(spiderman)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={spiderman} alt="spiderman drawing" onClick={() => openPopup(spiderman)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={trex} alt="trex drawing" onClick={() => openPopup(trex)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={trex} alt="trex drawing" onClick={() => openPopup(trex)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={toys} alt="toys drawing" onClick={() => openPopup(toys)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={toys} alt="toys drawing" onClick={() => openPopup(toys)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={tucan} alt="tucan drawing" onClick={() => openPopup(tucan)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={tucan} alt="tucan drawing" onClick={() => openPopup(tucan)} />
+//                 </div>
 
-                <div className='artwork'>
-                  <img src={yoshi} alt="yoshi drawing" onClick={() => openPopup(yoshi)} />
-                </div>
+//                 <div className='artwork'>
+//                   <img src={yoshi} alt="yoshi drawing" onClick={() => openPopup(yoshi)} />
+//                 </div>
 
-                {popupImage && (
-                  <div className="popup-overlay" onClick={closePopup}>
-                    <div className="popup-content">
-                      <img src={popupImage} alt="Popup" />
-                    </div>
-                  </div>
-                )}
+//                 {popupImage && (
+//                   <div className="popup-overlay" onClick={closePopup}>
+//                     <div className="popup-content">
+//                       <img src={popupImage} alt="Popup" />
+//                     </div>
+//                   </div>
+//                 )}
 
-              </div>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+//               </div>
+//             </div>
+//           </div>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
 
-export default Art;
+// export default Art;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,50 +2,31 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import resume from "../assets/Logan_Stack_Resume.pdf"
-import hand from '../assets/images/hand.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import resume from '../assets/Logan_Stack_Resume.pdf'
+import { ReactComponent as Github } from '../assets/images/github.svg'
+import { ReactComponent as Linkedin } from '../assets/images/linkedin.svg'
+import { ReactComponent as Mail } from '../assets/images/mail.svg'
 
-const Home = () => {
+export default function Home() {
   return (
-    <Container fluid className="vh-100 d-flex flex-column ">
-      <Row className="h-100">
-        <Col style={{ "margin": "auto" }} xs={{ order: 2, span: 12 }} md={{ order: 1, span: 6 }}>
-          <Row>
-            <div className="info_section home">
-              <p className="info_header">Hi, I'm Logan</p>
-              <p className="info_description">
-                My name's Logan and I'm a software engineer based in Detroit, MI with a passion for creativity.
-                I'm currently employed at General Motors, where I write software to test the cameras that go into
-                our autonomous vehicles. You can read about my past work experience below and can also checkout
-                my Github for links to personal projects.
-              </p>
-
-              <div id="links">
-                <a className="font_awesome_icons" href="mailto:loganestack@gmail.com" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </a>
-                <a className="font_awesome_icons" href="https://www.linkedin.com/in/logan-stack/" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} />
-                </a>
-                <a className="font_awesome_icons" href="https://github.com/LoganEStack" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-                {/* <a href={resume} target="_blank" rel="noopener noreferrer">
-                  <Button variant="outline-dark" className="rounded-0">Grab a resume</Button>
-                </a> */}
-              </div>
-            </div>
-          </Row>
-        </Col>
-        <Col className="home_section" xs={{ order: 1, span: 12 }} md={{ order: 2, span: 6 }}>
-          <img className="image_hand" src={hand} alt="Waving Hand"></img>
+    <Container fluid className="vh-100">
+      <Row>
+        <Col>
+          <header className="title" id="Logan">Logan</header>
+          <header className="title" id="Stack">Stack</header>
+          <div id="links">
+            <a href="mailto:loganestack@gmail.com" target="_blank" rel="noopener noreferrer">
+              <Mail width="2rem" height="auto"/>
+            </a>
+            <a href="https://www.linkedin.com/in/logan-stack/" target="_blank" rel="noopener noreferrer">
+              <Linkedin width="2rem" height="auto"/>
+            </a>
+            <a href="https://github.com/loganestack" target="_blank" rel="noopener noreferrer">
+              <Github width="2rem" height="auto"/>
+            </a>
+          </div>
         </Col>
       </Row>
     </Container>
   );
 };
-
-export default Home;
